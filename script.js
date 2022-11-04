5d2c6b5a843db2192f087470a18736e02504126c
MonarkGondaliya <mgondaliya@hawk.iit.edu>
2022-11-04 12:04:09 -0500

    Implemented overloading method for fetch weather

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
 
 let weather = {
-    featchWeather: function (city) {
-        var weatherUrl = "https://weatherdbi.herokuapp.com/data/weather/" + city
-        fetch(
-            weatherUrl)
+    featchWeather: function () {
+        if (arguments.length == 1) {
+            url = "https://weatherdbi.herokuapp.com/data/weather/" + arguments[0]
+            console.log(url)
+        }
+        else {
+            url = "https://weatherdbi.herokuapp.com/data/weather/" + arguments[0] + "," + arguments[1];
+            console.log(url)
+
+        } fetch(
+            url)
             .then(res => res.json())
             .then(data => this.displayWeather(data))
     },
@@ -25,9 +32,6 @@ let weather = {
         this.futureWeather(data)
 
 
-    },
-    searchLocation: function () {
-        this.featchWeather(document.querySelector(".searchBox").value)
     },
     futureWeather: function (data) {
 
@@ -50,11 +54,11 @@ let weather = {
     }
 }
 document.querySelector(".submit").addEventListener("click", function () {
-    weather.searchLocation();
+    weather.featchWeather(document.querySelector(".searchBox").value);
 })
 document.querySelector(".searchBox").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
-        weather.searchLocation();
+        weather.featchWeather(document.querySelector(".searchBox").value);
     }
 })
 weather.featchWeather("Chicago")
@@ -64,7 +68,6 @@ document.querySelector(".location").addEventListener("click", function () {
 })
 
 
-
 function success(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
@@ -72,53 +75,8 @@ function success(position) {
     console.log('Latitude is ' + latitude + '° Longitude is ' + longitude + '°')
     var locationUrl = "https://weatherdbi.herokuapp.com/data/weather/" + latitude + "," + longitude;
     console.log(locationUrl)
-    locationWeather.featchWeather(locationUrl)
+    weather.featchWeather(latitude,longitude)
 }
-
-let locationWeather = {
-    featchWeather: function (locationUrl) {
-        fetch(
-            locationUrl)
-            .then(res => res.json())
-            .then(data => this.displayWeather(data))
-    },
-    displayWeather: function (data) {
-        const { region } = data;
-        const { dayhour, precip, humidity, iconURL, comment } = data.currentConditions
-        const { c, f } = data.currentConditions.temp
-        const { km, mile } = data.currentConditions.wind
-        console.log(region, c, f, dayhour, precip, humidity, iconURL, comment, km, mile)
-        document.querySelector(".city").innerText = "Weather in " + region;
-        document.querySelector(".dayhour").innerText = dayhour;
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity;
-        document.querySelector(".temp").innerText = c + "°C/" + f + "°F";
-        document.querySelector(".precp").innerText = "Precipitation: " + precip;
-        document.querySelector(".wind").innerText = "Wind Speed: " + mile + " mile/hr";
-        document.querySelector(".icon").src = iconURL;
-        document.querySelector(".comment").innerText = comment;
-        this.futureWeather(data)
-    },
-    futureWeather: function (data) {
-
-        const result = JSON.stringify(data)
-        for (let i = 1; i < 7; i++) {
-            const { day, comment, iconURL } = data.next_days[i]
-            var min_c = data.next_days[i].min_temp.c
-            var min_f = data.next_days[i].min_temp.f
-            var max_c = data.next_days[i].max_temp.c
-            var max_f = data.next_days[i].max_temp.f
-
-            console.log(day, comment, iconURL, min_c, min_f, max_c, max_f)
-            document.querySelector(".day" + i).innerText = day;
-            document.querySelector(".icon" + i).src = iconURL;
-            document.querySelector(".comment" + i).innerText = comment;
-            document.querySelector(".min_temp" + i).innerText = "Min : " + min_c + "°C/" + min_f + "°F";
-            document.querySelector(".max_temp" + i).innerText = "Max : " + max_c + "°C/" + max_f + "°F";
-
-        }
-    }
-}
-
 
 function error() {
     location.innerHTML = "Unable to retrieve your location";
